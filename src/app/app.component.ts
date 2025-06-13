5e2d64b710f19e3cb5547f1f48a9ff7ee88d6285
zyndly <zyndlykentperlinbaja@gmail.com>
2025-06-13 17:27:06 +0800

    feat: Integrate SharedNavbarComponent and SharedFooterComponent into AppComponent for enhanced layout and dynamic title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
+import { SharedNavbarComponent } from './shared/shared-navbar/shared-navbar.component';
+import { SharedFooterComponent } from './shared/shared-footer/shared-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SharedNavbarComponent, SharedFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  title = environment.appName || 'akiro-coin';
+  title = environment.appName || 'Akiro-Coin';
   environmentName = '';
   apiUrl = '';
   
