95b6895effb28a24f06e51db89b7c8b289b0fa8d
zyndly <zyndlykentperlinbaja@gmail.com>
2025-06-13 16:43:02 +0800

    feat: Create SharedNavbarComponent with basic structure

---
+++ src/app/shared/shared-navbar/shared-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-navbar',
+  imports: [],
+  templateUrl: './shared-navbar.component.html',
+  styleUrl: './shared-navbar.component.css'
+})
+export class SharedNavbarComponent {
+
+}
