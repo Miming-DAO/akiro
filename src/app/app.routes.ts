66b692bf644eaf751e22c476e1a0b26055483e76
zyndly <zyndlykentperlinbaja@gmail.com>
2025-06-26 13:29:25 +0800

    feat: Add route for launch-app to display WorkOnProgressComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { WorkOnProgressComponent } from './shared/work-on-progress/work-on-progress.component';
 
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./features/landing/landing.routes').then(m => m.LANDING_ROUTES)
+  },
+  {
+    path: 'launch-app',
+    component: WorkOnProgressComponent
   },
   {
     path: '**',
